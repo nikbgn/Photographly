e84a63b39aa499c05570c7af35ea718536c74b7d
Nikolai <pythondevbg8@abv.bg>
2022-12-25 17:24:09 +0200

    Add project files.

---
+++ Photographly.Infrastructure/Data/PhotographlyDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Photographly.Infrastructure.Data
+{
+	using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore;
+
+	public class PhotographlyDbContext : IdentityDbContext
+	{
+		public PhotographlyDbContext(DbContextOptions<PhotographlyDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
---
+++ Photographly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Photographly.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
